9e75df6dc51dd2a5f73e099b1e45a599725d1294
mimof9 <1509945419@qq.com>
2018-12-28 16:34:58 +0800

    编写Web App骨架

---
+++ www/app.py
@@ -0,0 +1,17 @@
+
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from aiohttp import web
+from datetime import datetime
+
+def index(request):
+	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+def init():
+	app = web.Application()
+	app.router.add_route('GET', '/', index)
+	web.run_app(app, host='127.0.0.1', port=9000)
+	print('服务器启动，监听端口9000')
+
+init()
+
