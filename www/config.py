9d37dd67a11257d4ee252d1ac7b5f4fde4fd72bd
mimof9 <1509945419@qq.com>
2018-12-29 18:19:20 +0800

    配置文件

---
+++ www/config.py
@@ -0,0 +1,44 @@
+import config_default
+
+#就是支持直接.数据的dict
+class Dict(dict):
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+#用override中的值覆盖defaults中的值
+def merge(defaults, override):
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
+
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
+
+try:
+    import config_override
+    configs = merge(config_default.configs, config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs) #直接导入config模块 就可以得到配置
---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+#开发环境配置
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'admin',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
