6424de0e0ff0d89dce1e50dfc1a7370c2b06416d
mimof9 <1509945419@qq.com>
2019-01-01 16:53:45 +0800

    启动服务器的骨架

--- www/app.py
+++ www/app.py
@@ -108,6 +108,7 @@ async def response_factory(app, handler):
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:   #有模板，就当作html处理
+                r['__user__'] = request.__user__    #把拦截器处理的__user__ 自动加上
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
