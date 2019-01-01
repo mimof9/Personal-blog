fed987a21e6ec0b914661a1a444dcac0302b0a76
mimof9 <1509945419@qq.com>
2019-01-01 20:47:43 +0800

    服务器自用api 主要是封装的异常和分页类

--- www/apis.py
+++ www/apis.py
@@ -3,7 +3,7 @@ import json, logging, inspect, functools
 #分页 提供数据总数和分页大小 并指定第几页 然后Page就会计算sql的 offset和limit 并且判断是否有下一页或上一页
 class Page(object):
 
-    def __init__(self, item_count, page_index=1, page_size=10):
+    def __init__(self, item_count, page_index=1, page_size=5):
         self.item_count = item_count #数据总数
         self.page_size = page_size  #分页大小
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0) #需要页数
@@ -15,8 +15,8 @@ class Page(object):
             self.page_index = page_index
             self.offset = (page_index - 1) * page_size #当前页的偏移量 单位是数据条数
             self.limit = self.page_size #offset和limit主要是用来提供给 sql语句进行分页查询的
-        self.hash_next = self.page_index < self.page_count
-        self.hash_previous = self.page_index > 1
+        self.has_next = self.page_index < self.page_count
+        self.has_previous = self.page_index > 1
 
     def __str__(self):
         return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' \
